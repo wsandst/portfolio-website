03774f6db30fc51c6eb36a84c7dc68f91c3ad630
williamsandst <williamsandstrom99@hotmail.com>
2020-06-14 22:24:53 +0200

    Fixed issue with images on about page on mobile

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -45,7 +45,7 @@ class AboutPage extends React.Component {
           </div>
           <div css={css`margin-top: ${rhythm(2)};
           display: flex; flex-direction: row; 
-          @media (max-width: 620px) { max-width: 920px; margin-right: ${rhythm(1)}; flex-direction: column; justify-content: flex-end;}
+          @media (max-width: 620px) { max-width: 920px; margin-right: ${rhythm(1)}; flex-direction: column-reverse; justify-content: flex-end; margin-top: 1rem;}
           @media (max-width: 530px) { max-width: 920px; margin-right: 0; padding-right: 0;}`}>
           <div css={css`border-right: 1px solid var(--hr); padding-right: ${rhythm(1)};`}>
           <h1> Knowledge </h1>
@@ -76,15 +76,16 @@ class AboutPage extends React.Component {
             </span>
             </div>
             <div css={css`display: flex; justify-content: flex-end; margin-left: ${rhythm(1.5)}; margin-bottom: ${rhythm(1)};
-             @media (max-width: 620px) { display: none; margin: 0;}`}>
-               <img src={teaIcon} alt="tea" css={css`width: 150px; height: 150px; margin: auto; margin-right: 0; max-width: 160px;`}/>
+             @media (max-width: 620px) {  margin: 0; margin-left: auto; margin-right: auto; margin-bottom: 1rem; padding-left: ${rhythm(1)};}
+             @media (max-width: 530px) { padding-left: 0;}`}>
+               <img src={teaIcon} alt="tea" css={css`width: 160px; height: 160px; margin: auto; auto; max-width: 160px;`}/>
             </div>
           </div>
           <div css={css` margin-top: ${rhythm(2)}; display: flex; flex-direction: row;
           @media (max-width: 920px) {margin-left: 0}
           @media (max-width: 530px) {margin-left: 0}`}>
             <div css={css`margin-right: ${rhythm(1.5)}; display: flex; justify-content: flex-start; margin-bottom: ${rhythm(1)};
-              @media (max-width: 620px) { display: none; margin: 0;}`}>
+              @media (max-width: 620px) { margin: 0; display: none;}`}>
               <Img fixed={this.props.data.profile2.childImageSharp.fixed} css={css`margin: auto; max-width: 160px; border-radius: 50%;`}> </Img>
             </div>
             <div>
@@ -117,7 +118,7 @@ class AboutPage extends React.Component {
                 </span>
           </div>
           <div css={css`display: flex; justify-content: flex-end; margin-left: ${rhythm(1.5)}; margin-bottom: ${rhythm(1)};
-             @media (max-width: 620px) { display: none; margin: 0;}`}>
+             @media (max-width: 620px) { margin: 0; margin-left: auto; margin-right: auto; padding-left: ${rhythm(1)}; padding-top: ${rhythm(1.5)};}`}>
               <Img fixed={this.props.data.cat.childImageSharp.fixed} css={css`margin: auto; max-width: 160px; border-radius: 50%; margin-right: 0;`}> </Img>
             </div>
           </div>
