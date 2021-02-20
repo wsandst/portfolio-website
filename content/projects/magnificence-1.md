fea0b2cecc9e29d8781b5676d7e960be085f824e
wsandst <williamsandstrom99@hotmail.com>
2021-02-20 23:39:08 +0100

    Add profile pic back to front page for mobile website

--- content/projects/magnificence-1.md
+++ content/projects/magnificence-1.md
@@ -6,7 +6,7 @@ tags:
     - c++
     - chess
     - games
-description: "A UCI Chess Engine with an estimated ELO of 2000. This was my first larger C++ project made back in 2017 together with Harald Bjurulf. It uses an Alpha Beta Search with transposition tables and various other optimizations. The Board representation is a state of the art Bitboard with a move generation speed similar to Stockfish."
+description: "A UCI Chess Engine with an estimated ELO of 2000. This was my first larger C++ project made back in 2017 together with Harald Bjurulf. It uses an Alpha Beta Search with transposition tables and various other optimizations. The Board representation uses a Bitboard with a move generation speed similar to Stockfish."
 aim: "The aim of this project was to implement a functional UCI Chess Engine using Bitboards as well as collaborate together with Harald Bjurulf on our first larger C++ project."
 github: "https://github.com/wsandst/magnificence-chess"
 download: ""
--- src/components/AboutShort.jsx
+++ src/components/AboutShort.jsx
@@ -20,15 +20,15 @@ export default function AboutShort() {
     `)
     return (
       <div>
-        <div css={css`height:180px; margin-left: ${rhythm(2)};display: flex; justify-content: left;
-                    50%; @media (max-width: 630px) { margin-left:${rhythm(0.5) }}`}>
-          <div css={css`@media (max-width: 630px) { display: none; visibility: hidden; }`}>
+        <div css={css`height:180px; margin-left: ${rhythm(2)}; display: flex; flex-direction: row; justify-content: left;
+                    50%; @media (max-width: 630px) { margin-left:${rhythm(0) }; flex-direction: column-reverse; height:310px;  margin-top:  ${rhythm(0.6)}}`}>
+          <div css={css`@media (max-width: 630px) { margin-left: auto; margin-right: auto; padding-bottom: ${rhythm(0.5) }`}>
           <Link to={`/about`} alt="About page">
             <Img fixed={data.file.childImageSharp.fixed} css={css`border-radius: 50%;`} alt ="Profile Image"> </Img>
           </Link>
           </div>
           <div css={css`margin-left: ${rhythm(1.5)}; margin-bottom: ${rhythm(1)}; display: flex; flex-direction: column; justify-content: center; h2{margin:0}
-          @media (max-width: 550px) {margin-left: 0 }`}>
+          @media (max-width: 630px) {margin-left: auto; margin-right: auto; margin-bottom: ${rhythm(0.5)} }`}>
             <h2 css={css`padding-bottom: ${rhythm(0.2)}`}> Hello there. I'm William.</h2>
             <span>
             I'm a self-taught full-stack developer.
